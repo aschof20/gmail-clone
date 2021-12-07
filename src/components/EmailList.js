13ad53515be28e29313827c9e970c2f9e9a4d8a0
Alice Schofield <a.v.schofield@gmail.com>
2021-12-08 06:52:05 +1000

    Initial styling of email list component

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,10 +1,57 @@
 import React from 'react';
 import '../styles/EmailList.css';
+import Section from '../components/Section';
+import { Checkbox, IconButton } from '@material-ui/core';
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import RedoIcon from '@material-ui/icons/Redo';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardHideIcon from '@material-ui/icons/KeyboardHide';
+import SettingsIcon from '@material-ui/icons/Settings';
+import InboxIcon from '@material-ui/icons/Inbox';
+import PeopleIcon from '@material-ui/icons/People';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
 
 export default function EmailList() {
     return (
         <div className="emailList">
+            <div className="emailList_settings">
+                <div className="emailList_settingLeft">
+                    <Checkbox />
+                    <IconButton>
+                        <ArrowDropDownIcon />
+                    </IconButton>
+                    <IconButton>
+                        <RedoIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
 
+                </div>
+
+                <div className="emailList_settingRight">
+                    <IconButton>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChevronRightIcon />
+                    </IconButton>
+                    <IconButton>
+                        <KeyboardHideIcon />
+                    </IconButton>
+                    <IconButton>
+                        <SettingsIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="emailList_sections">
+                <Section Icon={InboxIcon} title='Primary' color='red' selected />
+                <Section Icon={PeopleIcon} title='Social' color='#1A73E8' />
+                <Section Icon={LocalOfferIcon} title='Promotions' color='green' />
+
+            </div>
         </div>
     );
 }
